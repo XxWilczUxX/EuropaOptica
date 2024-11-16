5f565b2b063f5212d731699a4d5ceb92c8ff19b8
Wiktor Radzikowski <wiktor.radzik102@gmail.com>
2024-11-16 17:45:00 +0100

    Implement dropdown hover

---
+++ JavaScript/navbar-desktop-hover.js
@@ -0,0 +1,28 @@
+const navbar = document.getElementById('navbar');
+const navItems = navbar.querySelectorAll('.nav-item');
+
+const dropdownContent = document.getElementById('dropdown-content');
+dropdownContent.style.top = `${navbar.offsetHeight}px`;
+
+navItems.forEach(item => {
+    item.firstChild.style.color = 'lightgray';
+    item.addEventListener('mouseover', () => { // Hovering
+        navbar.style.background = 'none';
+        navbar.style.backgroundColor = 'white';
+        navItems.forEach(element => {
+            element.firstChild.style.color = 'darkslategray';
+        });
+        item.firstChild.style.color = 'black';
+
+        dropdownContent.classList.add('show');
+    });
+    item.addEventListener('mouseout', () => { // Not hovering
+        navbar.style.backgroundColor = 'none';
+        navbar.style.background = 'linear-gradient(to bottom, black, transparent)';
+        navItems.forEach(element => {
+            element.firstChild.style.color = 'lightgray';
+        });
+
+        dropdownContent.classList.remove('show');
+    });
+});
