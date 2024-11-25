8708b9f742b7d4f37dc683dc670b896116a613f0
Wiktor Radzikowski <wiktor.radzik102@gmail.com>
2024-11-25 22:44:44 +0100

    Mobile navbar functionality

--- JavaScript/navbar-mobile.js
+++ JavaScript/navbar-mobile.js
@@ -1,75 +1,22 @@
-const navbarMobile = document.getElementById('navbar-mobile');
-const navMobileItems = navbarMobile.querySelectorAll('.nav-item');
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileNavbarHeight = document.querySelector('.mobile').offsetHeight;
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarCollapse = document.querySelector('#navbarNav');
+    var navbar = document.querySelectorAll('.navbar')[1];
 
-let isDown = false;
-
-const recalculateDropdown = () => {
-
-};
-
-const handleInsideClick = (item) => {
-    if (item.classList.contains('open')) {
-        item.children[1].style.display = 'none';
-        item.children[0].style.backgroundColor = 'white';
-        item.classList.remove('open');
-    }
-    else {
-        item.children[1].style.display = 'block';
-        item.children[0].style.backgroundColor = 'lightgray';
-        item.classList.add('open');
-    }
-
-    recalculateDropdown();
-}
-
-const handleMobileClick = (item) => {
-    if (isDown) {
-        navbarMobile.style.backgroundColor = 'none';
-        navbarMobile.style.background = 'linear-gradient(to bottom, black, transparent)';
-        navMobileItems.forEach(element => {
-            element.children[0].style.color = 'lightgray';
-        });
-
-        dropdownContent.classList.remove('show');
-        isDown = false;
-        return;
-    }
-    navbarMobile.style.background = 'none';
-    navbarMobile.style.backgroundColor = 'white';
-    navMobileItems.forEach(element => {
-        element.children[0].style.color = 'darkslategray';
-    });
-    item.children[0].style.color = 'black';
-
-    let contents = item.children[1].children;
-    dropdownContent.innerHTML = '';
-    dropdownContent.style.top = `${navbarMobile.offsetHeight}px`;
-    dropdownContent.style.minHeight = `calc(100vh - ${navbarMobile.offsetHeight}px)`;
+    navbarCollapse.style.minHeight = `calc(100vh - ${mobileNavbarHeight}px)`;
 
 
-    Array.from(contents).forEach(element => {
-        const clonedElement = element.cloneNode(true);
-        dropdownContent.appendChild(clonedElement);
-        attachSearchListeners(clonedElement);
+    navbarToggler.addEventListener('click', function () {
+
+        if (navbarCollapse.classList.contains('show')) {
+            navbar.classList.remove('navbar-expanded');
+        } else {
+            navbar.classList.add('navbar-expanded');
+        }
     });
 
-    contents = dropdownContent.querySelectorAll('.nav-item');
-    contents.forEach(element => {
-        element.addEventListener('click', () => {
-            handleInsideClick(element);
-        });
-    });
+    navbarCollapse.addEventListener('hidden.bs.collapse', function () {
 
-    recalculateDropdown();
-
-    dropdownContent.classList.add('show')
-    isDown = true;
-};
-
-navMobileItems.forEach(item => {
-    item.children[0].style.color = 'lightgray';
-
-    item.addEventListener('click', () => { // Clicked
-        handleMobileClick(item);
     });
 });
